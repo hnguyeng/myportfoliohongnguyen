2da3c0b919f28dd9c10715082a9e042ce1bc8c24
hnguyeng <quanghong22091999@gmail.com>
2023-08-03 15:44:04 +0700

    upload source code

---
+++ .history/assets/js/main_20230724094533.js
@@ -0,0 +1,73 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+/*======================= SWIPER PROJECTS ======================*/
+let swiperProjects = new Swiper(".projects__container", {
+    loop: true,
+    spaceBetween: 24,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    breakpoints: {
+        1200: {
+          slidesPerView: 2,
+          spaceBetween: -56,
+        },
+      },
+  });
+  // ========================SWIPER TESTIMONIAL======================
+  let swiperTestimonial = new Swiper(".testimonial__container", {
+    grabCursor: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+  // ======================EMAIL JS=====================
+  const contactForm = document.getElementById('contact-form'),
+        contactName = document.getElementById('contact-name'),
+        contactEmail = document.getElementById('contact-email'),
+        contactProject= document.getElementById('contact-project'),
+        contactMessage=document.getElementById('contact-message')
+  const sendEmail (e) => {
+    e.preventDefault()
+    // check if the field has a value 
+    if(contactName.value ==='' || contactEmail.value === '' || contactProject.value ===''){
+      // add and remove color
+      contactMessage.classList.remove('color-blue')
+      contactMessage.classList.add('color-red')
+
+      // show message 
+      
+    }
+  }  
